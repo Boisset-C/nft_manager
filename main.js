fab1b5ce28df60edadc92d7a464fb41c08673ee7
boisset-c <boissetcosme@gmail.com>
2021-12-15 18:28:16 -0600

    Able to fetch NFT data

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ serverUrl = "https://zhisezdhydkc.usemoralis.com:2053/server";
 appId = "4sjtOap670Lbm0FTcdJ88Gse8zS0QnjoF0TW1CkS";
 Moralis.start({ serverUrl, appId });
 
+//fetching from each NFT the metadata
+function fetchNFTMetadata(NFTs) {
+  for (let i = 0; i < NFTs.length; i++) {
+    let nft = NFTs[i];
+    let id = nft.token_id;
+    // cant make direct call to database, have to make a fetch to a cloud function which fetches the data
+    fetch(
+      "https://zhisezdhydkc.usemoralis.com:2053/server/functions/getNFT?_ApplicationId=4sjtOap670Lbm0FTcdJ88Gse8zS0QnjoF0TW1CkS&nftId=" +
+        id
+    )
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }
+}
+
 async function initializeApp() {
   let currentUser = Moralis.User.current();
 
@@ -14,8 +29,8 @@ async function initializeApp() {
     address: "0x61F3c00a76463c0a2fe6FEE6703819E7326B67e1",
     chain: "rinkeby",
   };
-  let NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
-  console.log(NFTs);
+  let NFTs = await Moralis.Web3API.token.getAllTokenIds(options); //grabbing nfts from contract address in options
+  fetchNFTMetadata(NFTs.result);
 }
 
 initializeApp();
